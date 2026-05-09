c71067012b7504af956f7e71880d9e2dc3066db4
Spikw285 <samsungcomtel139@gmail.com>
2026-05-09 12:59:04 +0500

    Updated config.py to include all event types.

--- src/config.py
+++ src/config.py
@@ -6,9 +6,14 @@ DATA_DIR = PROJECT_ROOT / "petrobras 3W main dataset"
 
 EVENTS = {
     0: "Normal",
+    1: "Abrupt Increase of BSW",
+    2: "Spurious Closure of DHSB",
     3: "Severe Slugging",
     4: "Flow Instability",
+    5: "Rapid Productivity Loss",
+    6: "Quick Restriction in PCK",
     7: "Scaling PCK",
+    8: "Hydrate in Production Line",
     9: "Hydrate in Service Line",
 }
 
